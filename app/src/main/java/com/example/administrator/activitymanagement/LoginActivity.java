e213914a1581c59bf5f66309d7788cdfe25c9e5e
CBJlove11 <2466152968@qq.com>
2018-12-25 19:59:27 +0800

    Login

--- app/src/main/java/com/example/administrator/activitymanagement/LoginActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/LoginActivity.java
@@ -2,12 +2,26 @@ package com.example.administrator.activitymanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+EditText ed_login_name,ed_login_password;
+CheckBox cb_login_Remember;
+Button btn_login_login,btn_login_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login();
+
+    }
+    private void login(){
+        ed_login_name=findViewById(R.id.ed_login_name);
+        ed_login_password=findViewById(R.id.ed_login_password);
+        cb_login_Remember=findViewById(R.id.cb_login_Remember);
+        btn_login_login=findViewById(R.id.btn_login_login);
+        btn_login_register=findViewById(R.id.btn_login_register);
     }
 }
