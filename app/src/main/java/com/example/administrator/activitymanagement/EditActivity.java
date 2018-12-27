16819096b6059f0f5f0daddcb225051e1800100c
stu <2466152968@qq.com>
2018-12-27 17:11:08 +0800

    4

--- app/src/main/java/com/example/administrator/activitymanagement/EditActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/EditActivity.java
@@ -72,7 +72,7 @@ EditText ed_edit_aName,ed_edit_aOpenTime,ed_edit_aEndTime,ed_edit_aPlace,ed_edit
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
 
-    }
+    }//
 
 
     }
