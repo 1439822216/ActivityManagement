f1dd39658cdee6beb27362c1632b793b26ce2c11
CBJlove11 <2466152968@qq.com>
2018-12-27 20:04:30 +0800

    GG

--- app/src/main/java/com/example/administrator/activitymanagement/EditActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/EditActivity.java
@@ -1,11 +1,10 @@
 package com.example.administrator.activitymanagement;
-
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Fragment;
-import android.os.Build;
-import android.support.annotation.Nullable;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+import android.support.annotation.Nullable;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,63 +18,63 @@ import java.util.Calendar;
 import java.util.List;
 
 public class EditActivity extends Fragment {
-EditText ed_edit_aName,ed_edit_aOpenTime,ed_edit_aEndTime,ed_edit_aPlace,ed_edit_aInfo;
+    EditText ed_edit_aName, ed_edit_aOpenTime, ed_edit_aEndTime, ed_edit_aPlace, ed_edit_aInfo;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_edit, container,false);
+        View view = inflater.inflate(R.layout.activity_edit, container, false);
         return view;
 
     }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //获取user对象
-        List<UserInfo> user = (List<UserInfo>)getArguments().getSerializable("user");
+        List<UserInfo> user = (List<UserInfo>) getArguments().getSerializable("user");
 
     }
-    public void initView(){
-        ed_edit_aName=getActivity().findViewById(R.id.ed_edit_aName);
-        ed_edit_aOpenTime=getActivity().findViewById(R.id.ed_edit_aOpenTime);
-        ed_edit_aEndTime=getActivity().findViewById(R.id.ed_edit_aEndTime);
-        ed_edit_aPlace=getActivity().findViewById(R.id.ed_edit_aPlace);
-        ed_edit_aInfo=getActivity().findViewById(R.id.ed_edit_aInfo);
 
+    public void initView() {
+        ed_edit_aName = getActivity().findViewById(R.id.ed_edit_aName);
+        ed_edit_aOpenTime = getActivity().findViewById(R.id.ed_edit_aOpenTime);
+        ed_edit_aEndTime = getActivity().findViewById(R.id.ed_edit_aEndTime);
+        ed_edit_aPlace = getActivity().findViewById(R.id.ed_edit_aPlace);
+        ed_edit_aInfo = getActivity().findViewById(R.id.ed_edit_aInfo);
+        ed_edit_aOpenTime.setInputType(InputType.TYPE_NULL);
         ed_edit_aOpenTime.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction()==MotionEvent.ACTION_DOWN){
-                    show1();
+                    showDatePickDlg();
                     return true;
                 }
                 return false;
             }
         });
+
         ed_edit_aOpenTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus){
-                    show1();
+                if(hasFocus){
+                    showDatePickDlg();
                 }
             }
         });
     }
-
-    protected void show1(){
+    protected void showDatePickDlg(){
         Calendar calendar = Calendar.getInstance();
-        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+        DatePickerDialog datePickerDialog=new DatePickerDialog(getActivity(),new OnDateSetListener(){
 
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            public  void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
                 EditActivity.this.ed_edit_aOpenTime.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
             }
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
-
     }
-
-
-    }
+}
 
 
 
--- app/src/main/java/com/example/administrator/activitymanagement/RegisterActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.activitymanagement;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
