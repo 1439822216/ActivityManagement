1657cb36c5c8570130e8420376b4f145959c7972
CBJlove11 <2466152968@qq.com>
2018-12-27 09:08:42 +0800

    Register

--- app/src/main/java/com/example/administrator/activitymanagement/RegisterActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/RegisterActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-EditText ed_reg_name,ed_reg_password,ed_reg_rpassword;
+EditText ed_reg_name,ed_reg_password,ed_reg_rpassword,ed_reg_vername,ed_reg_phone,ed_reg_class;
 Button btn_reg_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,9 @@ Button btn_reg_register;
         ed_reg_name=findViewById(R.id.ed_reg_name);
         ed_reg_password=findViewById(R.id.ed_reg_password);
         ed_reg_rpassword=findViewById(R.id.ed_reg_rpassword);
+        ed_reg_vername=findViewById(R.id.ed_reg_vername);
+        ed_reg_phone=findViewById(R.id.ed_reg_phone);
+        ed_reg_class=findViewById(R.id.ed_reg_class);
         btn_reg_register=findViewById(R.id.btn_reg_register);
     }
 }
