ba01ee447658c4fdcd5c11829f67e0068225e23c
CBJlove11 <2466152968@qq.com>
2018-12-28 16:58:36 +0800

    jj

--- app/src/main/java/com/example/administrator/activitymanagement/MeActivity.java
+++ app/src/main/java/com/example/administrator/activitymanagement/MeActivity.java
@@ -7,11 +7,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.activitymanagement.domain.UserInfo;
 
 public class MeActivity extends Fragment {
-
+TextView tv_me_name,tv_me_class,tv_me_phone,tv_me_End;
+ImageView im_me_image;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +27,10 @@ public class MeActivity extends Fragment {
         //获取user对象
          UserInfo user = (UserInfo)getArguments().getSerializable("user");
         Log.i("haha",user.toString());
-
+        tv_me_name=getActivity().findViewById(R.id.tv_me_name);
+        tv_me_class=getActivity().findViewById(R.id.tv_me_class);
+        tv_me_phone=getActivity().findViewById(R.id.tv_me_phone);
+        tv_me_End=getActivity().findViewById(R.id.tv_me_End);
+        im_me_image=getActivity().findViewById(R.id.im_me_image);
     }
 }
